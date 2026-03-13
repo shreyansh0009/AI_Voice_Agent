8d73a4f8bc7298745e65f105f4c9be48ee2abbdb
shreyansh0009 <saurabhsharraf@gmail.com>
2026-03-13 14:03:06 +0530

    changed uttrance to 1200

--- server/services/asteriskBridge.service.js
+++ server/services/asteriskBridge.service.js
@@ -615,7 +615,7 @@ class CallSession {
         smart_format: true,
         punctuate: true,
         interim_results: true,
-        utterance_end_ms: 800, // Triggers UtteranceEnd event
+        utterance_end_ms: 1200, // Triggers UtteranceEnd event
         vad_events: true,
         endpointing: 200,
       });
